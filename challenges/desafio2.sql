c21240cfc74122b3fa3c52c0746aa8abded218a0
DadAquino <david.aquino.exeutivo@gmail.com>
2023-05-09 23:32:35 -0300

    requisito 2

---
+++ challenges/desafio2.sql
@@ -0,0 +1,5 @@
+
+SELECT 
+(SELECT COUNT(song_id) FROM SpotifyClone.song) AS cancoes,
+(SELECT COUNT(artist_id) FROM SpotifyClone.artist) AS artistas,
+(SELECT COUNT(album_id) FROM SpotifyClone.album) AS albuns;
